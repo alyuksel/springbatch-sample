748fd31a5edd7a234c707b4cad91476a43df9525
Alparslan Yuksel <yuksel.alparslan.idf@gmail.com>
2019-11-08 17:35:03 +0100

    First commit

---
+++ src/main/java/com/alpi/ConsoleItemWriter.java
@@ -0,0 +1,13 @@
+package com.alpi;
+
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class ConsoleItemWriter<T> implements ItemWriter<T> {
+
+    @Override
+    public void write(List<? extends T> list) throws Exception {
+        list.forEach(System.out::println);
+    }
+}
